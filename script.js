796b684061cf5c269b12c5f5175e85efd8fb13c9
JasperLabo <138656214+JasperLabo@users.noreply.github.com>
2023-08-17 10:43:06 +0800

    labo

---
+++ script.js
@@ -0,0 +1,15 @@
+VANTA.WAVES({
+  el: ".page-wrapper, footer",
+  mouseControls: true,
+  touchControls: true,
+  gyroControls: false,
+  minHeight: 200.0,
+  minWidth: 200.0,
+  scale: 1.0,
+  scaleMobile: 1.0,
+  color: 0x193664,
+  shininess: 22.0,
+  waveHeight: 7.0,
+  waveSpeed: 0.75,
+  zoom: 0.76,
+});
